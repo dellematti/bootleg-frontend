f28be417728b7ce0d4a0f4d528f09d3cdadc41f4
dellematti <dellematti@hotmail.it>
2026-06-15 17:28:10 +0200

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,46 +6,9 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  styleUrl: './app.css',
   imports: [RouterOutlet, RouterLink, CommonModule],
-  template: `
-    <nav class="navbar">
-      <div class="nav-left">
-        <a routerLink="/" class="home-link">Homepage</a>
-      </div>
-
-      <div class="nav-right">
-        <ng-container *ngIf="authService.authVm$ | async as auth">
-
-          <!-- Stato non ancora inizializzato -->
-          <ng-container *ngIf="auth.initialized; else loading">
-
-            <!-- Utente loggato -->
-            <div *ngIf="auth.loggedIn; else loggedOut" class="user-section">
-              <span>Ciao {{ auth.user?.username }}!</span>
-              <a (click)="logout()" class="logout-link">Logout</a>
-            </div>
-
-            <!-- Utente non loggato -->
-            <ng-template #loggedOut>
-              <a routerLink="/login" class="login-link">Login</a>
-            </ng-template>
-
-          </ng-container>
-
-          <!-- Fase iniziale -->
-          <ng-template #loading>
-            <span class="loading-text">Caricamento...</span>
-          </ng-template>
-
-        </ng-container>
-      </div>
-    </nav>
-
-    <main class="content">
-      <router-outlet></router-outlet>
-    </main>
-  `
+  templateUrl: './app.component.html', 
+  styleUrl: './app.css'
 })
 export class App implements OnInit {
 
